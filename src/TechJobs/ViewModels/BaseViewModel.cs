7e1c8706af4e1d91a0e1ccaa70539a1137cefb59
Earl Gadel <earljg@gmail.com>
2018-06-16 15:43:03 -0500

    refactored code into BaseViewModel

--- src/TechJobs/ViewModels/BaseViewModel.cs
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -10,5 +10,18 @@ namespace TechJobs.ViewModels
     {
         public List<JobFieldType> Columns { get; set; }
         public string Title { get; set; } = "";
+
+        public BaseViewModel()
+        {
+            Columns = new List<JobFieldType>();
+
+            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
+            {
+                Columns.Add(enumVal);
+            }
+
+        }
     }
+
+
 }
--- src/TechJobs/ViewModels/SearchJobsViewModel.cs
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -26,18 +26,6 @@ namespace TechJobs.ViewModels
         // View title
         //public string Title { get; set; } = "";
 
-        public SearchJobsViewModel()
-        {
-            // Populate the list of all columns
-
-            Columns = new List<JobFieldType>();
-
-            foreach (JobFieldType enumVal in Enum.GetValues(typeof(JobFieldType)))
-            {
-                Columns.Add(enumVal);
-            }
-
-
-        }
     }
+    
 }
